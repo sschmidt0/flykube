ab343d5fe5bd777bb2db86b92642ee7a6ffa978e
sarah <sschmidt0@uoc.edu>
2022-01-07 11:15:43 +0100

    implemented buttons in upper-content with colour and text toggle, implemented console.log when component mounted

--- src/App.js
+++ src/App.js
@@ -1,12 +1,46 @@
 import logo from './logo.svg';
 import './App.scss';
+import { useState, useEffect } from 'react';
 
 export const App = () => {
+  const [activeButton, setActiveButton] = useState(true);
+
+  const handleButtonActiveState = () => {
+    // when button in upper-content is clicked bool status of activeButton is toggled
+    // so that colour and text of button changes
+    setActiveButton(!activeButton);
+  };
+
+  useEffect(() => {
+    console.log('Componente cargado correctamente');
+  }, []);
+
   return (
     <div className="App">
-      <div className="upper-content">buttons</div>
+      <div className="upper-content">
+        <div>
+            {/* dynamically adding 'active' className to change button colour */}
+            <button
+              className={ `${activeButton ? 'active' : null }` }
+              onClick={ handleButtonActiveState }
+            >
+              { activeButton ? 'Active' : 'Inactive' }
+            </button>
+          </div>
+          <div>
+            {/* !activeButton to get opposite effect from first button */}
+            <button
+              className={ `${!activeButton ? 'active' : null }` }
+              onClick={ handleButtonActiveState }
+            >
+              { !activeButton ? 'Active' : 'Inactive' }
+            </button>
+          </div>
+      </div>
       <div className="lower-content">
-        <div className="form">form</div>
+        <div className="form">
+          <button className="active">ENVIAR</button>
+        </div>
         <div className="logo">
           <img src={logo} className="App-logo" alt="logo" />
         </div>
